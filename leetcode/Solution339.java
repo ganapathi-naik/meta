27973a110d29b356daf0c88d3321320692b5eb75
Ganapathi Naik <6951518+ganapathi-naik@users.noreply.github.com>
2025-06-28 19:58:29 +0530

    339. Nested List Weight Sum.

---
+++ leetcode/Solution339.java
@@ -0,0 +1,17 @@
+class Solution339 {
+    public int depthSum(List<NestedInteger> nestedList) {
+        return dfs(nestedList, 1);
+    }
+
+    private int dfs(List<NestedInteger> nestedList), int level) {
+        int res = 0;
+        for(NestedInteger value : List<NestedInteger> nestedList) {
+            if(value.isInteger()) {
+                res += level * value.getInteger();
+            } else {
+                res += dfs(value.getList(), level + 1);
+            }
+        }
+        return res;
+    }
+}
