c1d32072305ed87150ce30bfbe8693c337d4a587
Ganapathi Naik <6951518+ganapathi-naik@users.noreply.github.com>
2025-07-08 13:40:37 +0530

    162. Find Peak Element.

--- leetcode/Solution162.java
+++ leetcode/Solution162.java
@@ -9,3 +9,23 @@ class Solution162 {
         return peakIndex;
     }
 }
+
+
+class Solution162 {
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            long toLeft = (mid - 1) < 0 ? Long.MIN_VALUE : nums[mid-1];
+            long toRight = (mid + 1) >= nums.length ? Long.MIN_VALUE : nums[mid + 1];
+            if(nums[mid] > toLeft && nums[mid] > toRight) {
+                return mid;
+            } else if(toLeft < nums[mid] && nums[mid] < toRight) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
