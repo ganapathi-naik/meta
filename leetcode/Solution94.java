c59b34a5f81755818e40daed3aa306225a9e7953
Ganapathi Naik <6951518+ganapathi-naik@users.noreply.github.com>
2025-07-09 19:54:49 +0530

    49. Group Anagrams.

---
+++ leetcode/Solution94.java
@@ -0,0 +1,13 @@
+class Solution94 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> anagrams = new HashMap<>();
+        for(String str : strs) {
+            char[] strArray = str.toCharArray();
+            Arrays.sort(strArray);
+            String sortedStr = new String(strArray);
+            anagrams.putIfAbsent(sortedStr, new ArrayList<>());
+            anagrams.get(sortedStr).add(str);
+        }
+        return new ArrayList<>(anagrams.values());
+    }
+}
