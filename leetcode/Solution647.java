df92b06ace16fb275fbebaa819820da49999e6bb
Ganapathi Naik <6951518+ganapathi-naik@users.noreply.github.com>
2025-07-13 12:12:27 +0530

    647. Palindromic Substrings.

--- leetcode/Solution647.java
+++ leetcode/Solution647.java
@@ -22,3 +22,28 @@ class Solution647 {
         return res;
     }
 }
+
+
+class Solution647 {
+    public int countSubstrings(String s) {
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int left = i;
+            int right = i;
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                res += 1;
+                left--;
+                right++;
+            }
+
+            left = i;
+            right = i + 1;
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                res += 1;
+                left--;
+                right++;
+            }
+        }
+        return res;
+    }
+}
