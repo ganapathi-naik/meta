e475b7a94207940da5b6dc91aa100a63a926e081
Ganapathi Naik <6951518+ganapathi-naik@users.noreply.github.com>
2025-07-21 21:08:09 +0530

    53. Maximum Subarray - Optimised solution.

--- leetcode/Solution53.java
+++ leetcode/Solution53.java
@@ -12,3 +12,31 @@ class Solution53 {
         return maxSubArray;
     }
 }
+
+
+class Solution53 {
+    public int maxSubArray(int[] nums) {
+        int maxEndingHere = 0;
+        int maxSoFar = nums[0];
+        int n = nums.length;
+        for(int i = 0; i < n; i++) {
+            maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
+}
+
+
+class Solution53 {
+    public int maxSubArray(int[] nums) {
+        int currSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currSum = Math.max(nums[i], currSum + nums[i]);
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;
+    }
+}
